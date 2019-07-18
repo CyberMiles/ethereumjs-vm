19aeb025e90b6becd34a7903c268325220517356
heysun0728 <heysun0728@gmail.com>
2019-07-18 11:41:42 +0000

    [Opcodes] Add fixed-point arithmetic Opcodes and their testing

--- tests/opFnsTest.js
+++ tests/opFnsTest.js
@@ -17,8 +17,6 @@ tape('test the integer arithmetic opcodes', function (test) {
   var c = new BN(U256UpperBound, 2) // -1
   var overflowError = new VmError(ERROR.OVERFLOW_ERROR)
 
-  test.plan(18)
-
   // UADD testing
   test.ok(opFns.UADD(b, b, null).cmp(a) === 0) // 1+1=2
   test.throws(function () { opFns.UADD(utils.TWO_POW256, b, null) }, overflowError)
@@ -48,6 +46,36 @@ tape('test the integer arithmetic opcodes', function (test) {
   test.ok(opFns.SMUL(b, c, null).cmp(c) === 0) // 1*(-1)=-1
   test.throws(function () { opFns.SMUL(S256UpperBound, a, null) }, overflowError)
   test.throws(function () { opFns.SMUL(S256LowerBound, c, null) }, overflowError)
+
+  test.end()
+})
+
+tape('test the fixed-point arithmetic opcodes', function (test) {
+  var a = new BN('10', 2) // 2
+  var b = new BN('01', 2) // 1
+  var c = new BN(U256UpperBound, 2) // -1
+  var zero = new BN('0', 2) // 0
+  var overflowError = new VmError(ERROR.OVERFLOW_ERROR)
+
+  // UFMUL testing
+  test.ok(opFns.UFMUL(a, b, b, null).cmp(zero) === 0) // x=2,y=1,N=1 0
+  test.ok(opFns.UFMUL(U256UpperBound, b, zero, null).cmp(U256UpperBound) === 0)
+  test.throws(function () { opFns.UFMUL(U256UpperBound, a, zero, null) }, overflowError)
+
+  // SFMUL testing
+  test.ok(opFns.SFMUL(a, c, b, null).cmp(zero) === 0) // x=2,y=-1,N=1 0
+  test.throws(function () { opFns.SFMUL(S256LowerBound, c, zero, null) }, overflowError)
+  test.throws(function () { opFns.SFMUL(S256UpperBound, a, zero, null) }, overflowError)
+
+  // UFDIV testing
+  test.ok(opFns.UFDIV(b, a, b, null).cmp(new BN('0101', 2)) === 0) // x=1,y=2,N=1 5
+  test.ok(opFns.UFDIV(U256UpperBound, b, zero, null).cmp(U256UpperBound) === 0)
+  test.throws(function () { opFns.UFDIV(U256UpperBound, a, a, null) }, overflowError)
+
+  // SFDIV testing
+  test.ok(opFns.SFDIV(c, b, zero, null).cmp(c) === 0) // x=-1,y=1,N=0 -1
+  test.throws(function () { opFns.SFDIV(S256LowerBound, c, zero, null) }, overflowError)
+  test.throws(function () { opFns.SFDIV(S256UpperBound, c, a, null) }, overflowError)
 
   test.end()
 })
